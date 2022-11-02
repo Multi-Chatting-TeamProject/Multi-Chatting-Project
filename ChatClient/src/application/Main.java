3f5e2d54c9061ee749813ec4fffed663495fb3bd
J0SEOYOUNG <49782140+J0SEOYOUNG@users.noreply.github.com>
2022-11-02 21:34:11 +0900

    Update Main.java

--- ChatClient/src/application/Main.java
+++ ChatClient/src/application/Main.java
@@ -184,7 +184,7 @@ public class Main extends Application {
 		
 		
 		// 사용자 이름이 들어갈 수 있는 텍스트 공간
-		String client = socket.getRemoteSocketAddress() + " ";
+		String client = socket.getLocalSocketAddress() + " ";
 		String clientNum = client.substring(11, 13);
 		Label userName = new Label(clientNum);
 		userName.setPrefWidth(50); // 너비
